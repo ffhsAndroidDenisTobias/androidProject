96cabf3b4ce2972b64bc2474ebb84ca579d5a76d
Denis Bittante <denisbittante@gmail.com>
2015-04-20 06:51:06 +0200

    mainpage card

--- MyUnihockey/app/src/main/java/myunihockey/ffhs/com/myunihockey/MainCard.java
+++ MyUnihockey/app/src/main/java/myunihockey/ffhs/com/myunihockey/MainCard.java
@@ -17,15 +17,13 @@ public class MainCard extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpagecard);
-
-
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_teams, menu);
+        getMenuInflater().inflate(R.menu.menu_maincard, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
